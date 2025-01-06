82df3ea22934dbe6d03a9618eecff9b02e226c6a
N.I <iworksWeb@gmail.com>
2025-01-06 22:31:55 +0900

    add TaskCard.tsx

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -1,5 +1,17 @@
+import Task from "../Task/Task";
+import TaskAddInput from "../TaskAddInput/TaskAddInput";
+import TaskCardDeleteBtn from "../TaskCardDeleteBtn/TaskCardDeleteBtn";
+import TaskCardTitle from "../TaskCardTitle/TaskCardTitle";
+
 const TaskCard = () => {
-  return <div>TaskCard</div>;
+  return (
+    <div className="taskCard">
+      <TaskCardTitle />
+      <TaskCardDeleteBtn />
+      <TaskAddInput />
+      <Task />
+    </div>
+  );
 };
 
 export default TaskCard;

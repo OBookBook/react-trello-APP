7c1cbd0e1b3d1ae0191ae5f9588f0526bc75f1ca
N.I <iworksWeb@gmail.com>
2025-01-06 23:16:05 +0900

    add TaskAddInput.tsx

--- src/components/TaskAddInput/TaskAddInput.tsx
+++ src/components/TaskAddInput/TaskAddInput.tsx
@@ -1,5 +1,16 @@
 const TaskAddInput = () => {
-  return <div>TaskAddInput</div>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="add a task" className="taskAddInput" />
+      </form>
+    </div>
+  );
 };
 
 export default TaskAddInput;
